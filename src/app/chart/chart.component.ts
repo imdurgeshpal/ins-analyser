9fe7d72c8a06424cada8e871093918667e6e2f9d
Durgesh Pal <durgesh.pal@soprasteria.com>
2021-12-18 23:29:16 +0530

    transform data to show chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import * as Highcharts from 'highcharts';
+import * as Highcharts from "highcharts/highstock";
+import { Options, SeriesOptionsType } from 'highcharts/highstock';
+import { mockStockMarket } from '../shared/models/mock-share-market';
+import { ShareMarket } from '../shared/models/share-market';
 
 @Component({
   selector: 'app-chart',
@@ -8,31 +11,82 @@ import * as Highcharts from 'highcharts';
 })
 export class ChartComponent implements OnInit {
 
+  highcharts: typeof Highcharts = Highcharts;
+  seriesOptions: Array<SeriesOptionsType> = [];
+  seriesCounter = 0;
+  names = ['MSFT', 'AAPL', 'GOOG'];
+  private shareMarkets: ShareMarket[] = mockStockMarket;
 
-  highcharts = Highcharts;
-
-  chartOptions: Highcharts.Options = {
-    title: {
-      text: "Infosys stock value"
-    },
-    xAxis: {
-      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    },
-    yAxis: {
-      title: {
-        text: "Infosys Stock value in dollar"
-      }
-    },
-    series: [{
-      data: [12, 8, 43, 35, 20, 90, 100, 110],
-      type: 'line'
-    }]
-  }
+  chartOptions: Options;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tranformData();
   }
 
+  private tranformData() {
+    const currentDate = new Date().getTime();
+    const oneMonthAgo = new Date(new Date().setMonth(new Date().getMonth() - 1)).getTime();
+    const sixMonthAgo = new Date(new Date().setMonth(new Date().getMonth() - 6)).getTime();
+    const oneYearAgo = new Date(new Date().setMonth(new Date().getMonth() - 12)).getTime();
+    const fiveYearAgo = new Date(new Date().setMonth(new Date().getMonth() - 60)).getTime();
+
+    this.shareMarkets.forEach((market, i) => {
+      if (i < 5) {
+        const arr = [];
+        const convertPrice = (price: string): number => +(price.replace(/(^\$|,)/g, ''));
+        arr.push(
+          [currentDate, convertPrice(market.currentPrice)],
+          [oneMonthAgo, convertPrice(market.price1MonthAgo)],
+          [sixMonthAgo, convertPrice(market.price6MonthsAgo)],
+          [oneYearAgo, convertPrice(market.price1YearAgo)],
+          [fiveYearAgo, convertPrice(market.price5YearsAgo)]
+        );
+        this.seriesOptions.push(
+          {
+            name: market.company,
+            data: arr,
+            type: 'line'
+          }
+        )
+      }
+    });
+    this.createChart();
+  }
+
+
+  private createChart() {
+    this.chartOptions = {
+      rangeSelector: {
+        selected: 5
+      },
+      yAxis: {
+        labels: {
+          formatter: function () {
+            return (this.value > 0 ? ' + ' : '') + this.value + '%';
+          }
+        },
+        plotLines: [{
+          value: 0,
+          width: 2,
+          color: 'silver'
+        }]
+      },
+
+      plotOptions: {
+        series: {
+          compare: 'percent',
+          showInNavigator: true
+        }
+      },
+
+      tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+        valueDecimals: 2,
+        split: true
+      },
+      series: this.seriesOptions
+    };
+  }
 }
